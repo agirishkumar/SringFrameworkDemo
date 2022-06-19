1270bd6167431e4684f43783d9f182b760492ea2
agirishkumar <adari.girishkumar@gmail.com>
2022-06-19 11:25:56 +0530

    Spring method of dependency injection

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -9,18 +9,10 @@ public class Main {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-
-//        Doctor doctor = new Doctor();
         Doctor doctor = context.getBean(Doctor.class);
         doctor.assist();
-        System.out.println(doctor.getQualification());
         Nurse nurse = context.getBean(Nurse.class);
         nurse.assist();
-        Staff staff = context.getBean(Nurse.class);
-        staff.assist();
-        Staff staff2 = context.getBean(Doctor.class);
-        staff2.assist();
-
 
     }
 }
