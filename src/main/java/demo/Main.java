ff65b40f0b8c8cfb6df516670bc299bfb3ca7d56
agirishkumar <adari.girishkumar@gmail.com>
2022-06-19 11:32:23 +0530

    Java configuration method of dependency injection

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,18 +1,20 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 
         Doctor doctor = context.getBean(Doctor.class);
         doctor.assist();
         Nurse nurse = context.getBean(Nurse.class);
         nurse.assist();
+        Staff staff = context.getBean(Doctor.class);
+        staff.assist();
 
     }
 }
